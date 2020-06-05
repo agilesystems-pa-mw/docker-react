1bd81466213d626ab005b7a16cf55d6c8fd4d743
Alinafe Mbendera <alinafe@Alinafes-MacBook-Pro.local>
2020-06-05 13:40:01 +0200

    added travis file

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Useless People - Welcome lel
+          learn react
         </a>
       </header>
     </div>
